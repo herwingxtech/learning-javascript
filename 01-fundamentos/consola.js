cde0004180878ef99d9c2db7e74a3540ee395049
herwingx-work <herwingmacias@gmail.com>
2025-01-29 09:18:28 -0600

    Reorganiza el código de consola: renombra app.js a consola.js

---
+++ 01-fundamentos/consola.js
@@ -0,0 +1,14 @@
+console.log("Hola Mundo!");
+let a = 10, b = 20, c = "Hola ", d = "Spiderman", x = a + b;
+
+console.log({ a });
+console.log({ b });
+console.log({ c });
+console.log({ d });
+console.log({ x });
+
+
+console.table({ a, b, c, d, x });
+
+const saludo = c + d;
+console.log(saludo);
