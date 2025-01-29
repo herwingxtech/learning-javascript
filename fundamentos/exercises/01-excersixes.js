4076dae3bbbeb1e351e6e07b825fa41d1e4e549d
herwingx-work <herwingmacias@gmail.com>
2025-01-29 15:36:07 -0600

    Añade ejercicios de fundamentos sobre tipos de datos primitivos en JavaScript

---
+++ fundamentos/exercises/01-excersixes.js
@@ -0,0 +1,91 @@
+/*
+Clase 18 - Ejercicios: primeros pasos
+Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
+*/
+
+// 1. Escribe un comentario en una línea
+
+//Hola esto es un comentario en una línea
+
+// 2. Escribe un comentario en varias líneas
+
+/* 
+Este es un comentario en varias líneas
+*/
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+
+let name = "Eduardo"; // String
+
+let number = 10; // Number
+
+let boolean = true; // Boolean
+
+let undefined; // Undefined
+
+let symbol = Symbol("b"); // Symbol
+
+let bigInt = 10n; // BigInt
+
+let nullValue = null; // Null
+
+
+// 4. Imprime por consola el valor de todas las variables
+
+console.log(name)
+
+console.log(number)
+
+console.log(boolean)
+
+console.log(undefined);
+
+console.log(symbol)
+
+console.log(bigInt)
+
+console.log(nullValue)
+
+// 5. Imprime por consola el tipo de todas las variables
+console.log(typeof (name))
+
+console.log(typeof (number))
+
+console.log(typeof (boolean))
+
+console.log(typeof (undefined));
+
+console.log(typeof (symbol))
+
+console.log(typeof (bigInt))
+
+console.log(typeof (nullValue))
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+name = "Herwing";
+number = 15;
+boolean = false;
+// undefined = ;
+symbol = Symbol("c");
+bigInt = 20n;
+// nullValue = ;
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const nameConst = "Eduardo"; // String
+
+const numberConst = 10; // Number
+
+const booleanConst = true; // Boolean
+
+// const undefinedConst; // Undefined
+
+const symbolConst = Symbol("b"); // Symbol
+const bigIntConst = 10n; // BigInt
+
+const nullValueConst = null; // Null
+
+
+// 9. A continuación, modifica los valores de las constantes
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
