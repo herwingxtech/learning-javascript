eb95891d34e1355ab21700014f4476115d074c7b
Eduardo Macias <herwingmacias@gmail.com>
2025-08-26 16:09:38 -0600

    Corrige el valor de 'dia' en value-reference.js y mejora la legibilidad del código al agregar espacios en blanco entre las secciones.

--- fundamentos/value-reference.js
+++ fundamentos/value-reference.js
@@ -18,16 +18,27 @@ si yo asigno un objeto a una variable y luego asigno esa variable a otra
 variable, ambas variables apuntan al mismo objeto en memoria.
 Por lo tanto, si modifico el objeto a través de una de las variables, 
 la otra variable también reflejará ese cambio. */
-dia = 5;
+dia = 1;
 
 const diaLetras = {
+    0: 'Domingo', 
     1: 'Lunes',
     2: 'Martes',
     3: 'Miercoles',
     4: 'Jueves',
     5: 'Viernes',
     6: 'Sabado',
-    7: 'Domingo' 
 }
 
 console.log(diaLetras[dia]);
+
+const horaActual = 10;
+
+
+horaApertura = ([0,6].includes(dia)) ? 9 : 11;
+
+mensaje = (horaActual >= horaApertura) ? 'Esta abierto' : `Esta cerrado, hoy abrimos a las ${horaApertura}`;
+
+
+
+console.log({horaApertura, mensaje});
